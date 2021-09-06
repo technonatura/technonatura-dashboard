d3bbc8a75af06b0bf26a19b7a9f90939b60e74c4
aldhanekaa <aldhanekadev@gmail.com>
2021-09-06 15:31:18 +0700

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -100,6 +100,11 @@ const nextConfig = {
 
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   env,
 };
 
